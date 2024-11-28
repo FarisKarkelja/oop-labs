0ded421acabb61b0cb6e3604668bb8a25550509d
Faris Karkelja <faree5803@gmail.com>
2024-11-28 14:09:49 +0100

    week 8

--- src/main/java/week8/CarManagementSystem.java
+++ src/main/java/week8/CarManagementSystem.java
@@ -322,25 +322,19 @@ class RentalCompany {
 class MainProgram {
     public static void main(String[] args) {
         RentalCompany company = new RentalCompany("Advanced Rentals");
-        Car car = new Car("C001", "Toyota Corolla", 50, CarType.SEDAN,
-                "Petrol");
-        Motorcycle motorcycle = new Motorcycle("M001", "Yamaha R3",
-                30, 321);
+        Car car = new Car("C001", "Toyota Corolla", 50, CarType.SEDAN, "Petrol");
+        Motorcycle motorcycle = new Motorcycle("M001", "Yamaha R3", 30, 321);
         company.addVehicle(car);
         company.addVehicle(motorcycle);
-        CustomerV2 customer = new CustomerV2("CU001", "Alice Smith",
-                "123-456-7890");
-        RentalTransaction<Car> carRental = new
-                RentalTransaction<>("T001", new Date(), 1);
+        CustomerV2 customer = new CustomerV2("CU001", "Alice Smith", "123-456-7890");
+        RentalTransaction<Car> carRental = new RentalTransaction<>("T001", new Date(), 1);
         carRental.addVehicleToRental(car, 3);
-        RentalTransaction<Motorcycle> motorcycleRental = new
-                RentalTransaction<>("T002", new Date(), 1);
+        RentalTransaction<Motorcycle> motorcycleRental = new RentalTransaction<>("T002", new Date(), 1);
         motorcycleRental.addVehicleToRental(motorcycle, 5);
         customer.addRentalTransaction(carRental);
         customer.addRentalTransaction(motorcycleRental);
         company.addCustomer(customer);
         company.displayInventory();
-        System.out.println("Total Revenue: $" +
-                company.calculateTotalRevenue());
+        System.out.println("Total Revenue: $" + company.calculateTotalRevenue());
     }
 }
